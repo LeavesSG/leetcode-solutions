8790ef52a0e20e6ea2b2f2d60ec522ac6a6e35a4
Natsuha SG <leaves_sg@outlook.com>
2025-01-15 17:02:10 +0800

    q(147): change impl to use flat vec

--- src/solutions/q174_dungeon_game/mod.rs
+++ src/solutions/q174_dungeon_game/mod.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 /*
  * @lc app=leetcode id=174 lang=rust
  *
@@ -11,32 +13,35 @@ impl Solution {
     pub fn calculate_minimum_hp(dungeon: Vec<Vec<i32>>) -> i32 {
         let m = dungeon.len();
         let n = dungeon.first().unwrap().len();
-        let mut min_health_at = std::collections::HashMap::new();
-
-        min_health_at.insert((m - 1, n - 1), 1);
+        let mut min_health_at = vec![0; m * n];
+        min_health_at.fill(0);
+        let rc_to_index = |r: usize, c: usize| r * n + c;
+        min_health_at[rc_to_index(m - 1, n - 1)] = 1;
         let mut q = std::collections::VecDeque::from(vec![(m - 1, n - 1)]);
         while let Some((r, c)) = q.pop_front() {
             let room = dungeon[r][c];
-            let prerequisite = min_health_at.get(&(r, c)).unwrap();
+            let prerequisite = min_health_at[rc_to_index(r, c)];
             let required = std::cmp::max(1, prerequisite - room);
 
             [(r.checked_sub(1), Some(c)), (Some(r), c.checked_sub(1))]
                 .into_iter()
                 .for_each(|(r, c)| {
                     if let (Some(r), Some(c)) = (r, c) {
-                        if let Some(req) = min_health_at.get(&(r, c)) {
-                            if required < *req {
-                                min_health_at.insert((r, c), std::cmp::min(*req, required));
+                        let idx = rc_to_index(r, c);
+                        let req = min_health_at[idx];
+                        if req != 0 {
+                            if required < req {
+                                min_health_at[idx] = std::cmp::min(req, required);
                                 q.push_back((r, c));
                             }
                         } else {
-                            min_health_at.insert((r, c), required);
+                            min_health_at[idx] = required;
                             q.push_back((r, c));
                         }
                     }
                 });
         }
-        std::cmp::max(1, min_health_at.get(&(0, 0)).unwrap() - dungeon[0][0])
+        std::cmp::max(1, min_health_at[0] - dungeon[0][0])
     }
 }
 // @lc code=end
@@ -44,7 +49,7 @@ impl Solution {
 #[test]
 fn test() {
     use crate::vecnd;
-    let dungeon = vecnd![[-2, -3, 3], [-5, -10, 1], [10, 30, -5]];
+    let dungeon = vecnd![[2], [1]];
     let result = Solution::calculate_minimum_hp(dungeon);
     println!("{}", result);
 }
